5971bb3758c96e9d5d908ce6de893d32ed39766d
Aika-Toki <89883773+aika-n-toki@users.noreply.github.com>
2024-01-12 17:47:46 +0900

    commit

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ document.querySelector("#diceB").addEventListener("click", addDiceB, false);
 document.querySelector("#diceC").addEventListener("click", addDiceC, false);
 document.querySelector("#diceD").addEventListener("click", addDiceD, false);
 document.querySelector("#roll").addEventListener("click", roll, false);
+function playDiceSound() {
+  let whichSound = Math.floor(Math.random() * 2);
+  let audio;
+  if (whichSound == 0) {
+    audio = new Audio("./sounds/dice1.mp3");
+  } else {
+    audio = new Audio("./sounds/dice2.mp3");
+  }
+  audio.play();
+}
 
 function addDiceA() {
   addDice(6);
@@ -38,8 +48,30 @@ function generateDice(face) {
 }
 function roll() {
   let dice = [...document.querySelectorAll("div.dice")];
-  dice.forEach((e) => {
-    e.querySelector(".face").textContent = Math.ceil(Math.random() * Number(e.dataset.max));
-  });
-  document.querySelector(".total>span").textContent = eval([...document.querySelectorAll(".face")].map((e) => Number(e.textContent)).join("+"));
+  let maxRollTime = 15;
+  let rollTime = 0;
+  let soundCount = 0;
+  let wait = 80;
+  let soundInterval = setInterval(() => {
+    soundCount++;
+    playDiceSound();
+    if (soundCount > Math.floor(dice.length / 2)) {
+      clearInterval(soundInterval);
+    }
+  }, 120);
+  document.querySelector(".total>span").textContent = "";
+  document.querySelector(".roll-btn").classList.add("hidden");
+  let interval = setInterval(() => {
+    rollTime++;
+    dice.forEach((e) => {
+      e.querySelector(".face").textContent = Math.ceil(Math.random() * Number(e.dataset.max));
+    });
+    if (rollTime > maxRollTime) {
+      clearInterval(interval);
+    }
+  }, wait);
+  setTimeout(() => {
+    document.querySelector(".total>span").textContent = eval([...document.querySelectorAll(".face")].map((e) => Number(e.textContent)).join("+"));
+    document.querySelector(".roll-btn").classList.remove("hidden");
+  }, wait * (maxRollTime + 1));
 }
